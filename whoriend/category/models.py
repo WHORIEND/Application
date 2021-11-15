a61e39bd716a85e247d850c124f1dc3517e49330
sanghyeon <tjtkdgus45@nate.com>
2021-11-16 04:00:41 +0900

    add category, message, reviews database schema

--- whoriend/category/models.py
+++ whoriend/category/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.db.models.deletion import CASCADE
+from django.db.models.fields.related import ForeignKey
 
 # Create your models here.
+
+class category(models.model):
+    name = models.CharField(max_length=30, primary_key=True)
+
+class detail_category(models.model):
+    cid = models.CharField(max_length=30, primary_key=True)
+    name = models.ForeignKey("category.name", on_delete=CASCADE)
+    detail_name = models.CharField(max_length=30)
--- whoriend/reviews/models.py
+++ whoriend/reviews/models.py
@@ -1,3 +1,12 @@
 from django.db import models
-
+from django.contrib.auth.models import AbstractUser
 # Create your models here.
+
+star_range = zip(range(1, 5), range(1, 5))
+
+class Review(AbstractUser):
+    Time_Compliance = models.IntegerField(choices=star_range, blank=True) #1~5값만 가능
+    Good_Teaching = models.IntegerField(choices=star_range, blank=True) #1~5값만 가능
+    Nice_Value = models.IntegerField(choices=star_range, blank=True) #1~5값만 가능
+    UID = models.ForeignKey("users", related_name = "users", on_delete=models.CASCADE, db_column="UID")
+    
